936b04b8199f55ea2ff0656ff63ca4d7c2624383
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-26 21:32:32 +0530

    configure and create backend server for api service

---
+++ apps/api-server/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import app from './src/app.ts';
+import http from 'http';
+
+const port = process.env.PORT ?? 4000;
+
+const server = http.createServer(app);
+
+
+server.listen(port, () => {
+    console.log(`Server is up and running on http://localhost:${port}`);
+});
