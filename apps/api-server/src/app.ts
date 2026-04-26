87bfa2ceff5afa27bce1134c9fb78c117ba5bb6b
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-26 21:35:28 +0530

    Add health check route in api server

--- apps/api-server/src/app.ts
+++ apps/api-server/src/app.ts
@@ -2,8 +2,16 @@ import express, { type Request, type Response } from 'express';
 
 const app = express();
 
+
 app.get('/', (req: Request, res:Response) => {
     return res.send("Hello express");
+});
+
+app.get('/health', (req:Request, res: Response) => {
+    res.status(200).json({
+        message: "Don't worry, I am healthy",
+        success: true 
+    });
 });
 
 export default app;
