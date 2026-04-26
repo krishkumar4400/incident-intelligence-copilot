ab553594a4374e5864cc1213de9b4284e6669e61
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-26 21:58:56 +0530

    Add initial files and folders

--- apps/api-server/server.ts
+++ apps/api-server/server.ts
@@ -6,7 +6,6 @@ const port = process.env.PORT ?? 4000;
 
 const server = http.createServer(app);
 
-
 server.listen(port, () => {
     console.log(`Server is up and running on http://localhost:${port}`);
 });
